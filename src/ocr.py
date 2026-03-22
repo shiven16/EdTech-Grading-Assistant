01e07ac18c262bb65b6c9126d33934be87d6c79f
mannvaswani4 <mannvaswani4@gmail.com>
2026-03-22 11:46:44 +0530

    update OCR module to classical OCR

--- src/ocr.py
+++ src/ocr.py
@@ -1,9 +1,25 @@
-import easyocr
+import pytesseract
+import cv2
 
-# Initialize once (important for performance)
-reader = easyocr.Reader(['en'])
+
+
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+
+    # convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # apply thresholding (important for OCR)
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+    return thresh
+
+
 
 def extract_text_from_image(image_path):
-    result = reader.readtext(image_path, detail=0)
-    text = " ".join(result)
+    processed_img = preprocess_image(image_path)
+
+    # pytesseract expects PIL image or array
+    text = pytesseract.image_to_string(processed_img)
+
     return text
